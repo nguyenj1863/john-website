3756b57e933a2ec33c887b96c4c666ea05b7ce9f
John Nguyen <nguyenj1863@gmail.com>
2025-07-29 10:01:51 -0400

    Push my actual Next.js project

--- src/app/work/projects/Website.js
+++ src/app/work/projects/Website.js
@@ -3,12 +3,11 @@ import SectionHeading from "../SectionHeading";
 export default function Website() {
     return (
         <div className="mt-4 space-y-6">
-            <div className="mb-6">
+            {/* <div className="mb-6">
                 <SectionHeading>Technical Overview</SectionHeading>
                 <p className="font-body font-light mb-2">
-                    Building the website involved:
                 </p>
-            </div>
+            </div> */}
         </div>
     );
 }
